1b7c325ff70c13e4a5673fce0e53f9d2b9eaa24b
EbiEEYY <pupil26_sea@school52.kiev.ua>
2025-07-05 17:47:06 +0300

    g

---
+++ src/js/home/hero/products.js
@@ -0,0 +1,6 @@
+// const list = document.querySelector(".products__list");
+
+// list.addEventListener("click", (e)=>{
+// console.log(e.target);
+// console.log(e.currentTarget);
+// })
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import '../styles/main.scss';
+
+const ref = {
+  arr_trigger: document.querySelector('.arr-trigger'),
+  arr_container: document.querySelector('.arr-container'),
+};
+ref.arr_trigger.addEventListener('mouseenter', () => {
+  ref.arr_container.classList.add('active');
+});
+
+ref.arr_trigger.addEventListener('mouseleave', () => {
+  ref.arr_container.classList.remove('active');
+});
