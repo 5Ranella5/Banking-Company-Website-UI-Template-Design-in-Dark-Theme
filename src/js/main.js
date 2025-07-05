e442fb5c3e7a6a30925cca7130f67f5d39e17802
EbiEEYY <pupil26_sea@school52.kiev.ua>
2025-07-05 23:41:06 +0300

    n

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,29 @@
 import '../styles/main.scss';
 
-const ref = {
-  arr_trigger: document.querySelector('.arr-trigger'),
-  arr_container: document.querySelector('.arr-container'),
-};
-ref.arr_trigger.addEventListener('mouseenter', () => {
-  ref.arr_container.classList.add('active');
-});
+// const ref = {
+//   arr_trigger: document.querySelector('.arr-trigger'),
+//   arr_container: document.querySelector('.arr-container'),
+// };
+// ref.arr_trigger.addEventListener('mouseenter', () => {
+//   ref.arr_container.classList.add('active');
+// });
 
-ref.arr_trigger.addEventListener('mouseleave', () => {
-  ref.arr_container.classList.remove('active');
-});
+// ref.arr_trigger.addEventListener('mouseleave', () => {
+//   ref.arr_container.classList.remove('active');
+// });
+
+
+//////////////////////////////////////////products
+
+// const buttons = document.querySelector(".buttons");
+
+// buttons.addEventListener("click", (e)=>{
+// console.log(e.target);
+// console.log(e.currentTarget);
+// })
+
+//////////////////////////////////////////features
+
+
+
+
